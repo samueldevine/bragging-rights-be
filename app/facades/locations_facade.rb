74d93c97ddc8601894ccc3782f8767eb1a3ba622
Sam Devine <samuel.will.devine@gmail.com>
2022-01-06 17:50:22 -0700

    Add locations service

--- app/facades/locations_facade.rb
+++ app/facades/locations_facade.rb
@@ -1,5 +1,5 @@
 class LocationsFacade
   def self.location_data(ip_address)
-    Location.new(LocationsService.location_data(ip_address))
+    Location.new(LocationsService.parsed_location_data(ip_address))
   end
 end
