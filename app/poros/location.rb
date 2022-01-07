6441756d0e1397708e0696d9cc248056afb1c150
Sam Devine <samuel.will.devine@gmail.com>
2022-01-06 18:49:28 -0700

    add 'no_id' attribute to location

--- app/poros/location.rb
+++ app/poros/location.rb
@@ -1,9 +1,11 @@
 class Location
-  attr_reader :city,
+  attr_reader :id,
+              :city,
               :state,
               :country
 
   def initialize(location_data)
+    @id      = 'no_id'
     @city    = location_data[:city]
     @state   = location_data[:regionName]
     @country = location_data[:country]
