003dc22c6ee835d5de696c10b61d80b67b5692a3
Christian Valesares <cvalesares@gmail.com>
2022-01-09 13:57:56 -0500

    add spec and method for top scores by location endpoint for FE

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ApplicationRecord
+  def self.top_scores_by_location(geo_scope, user_location)
+    Game.where("#{geo_scope} = '#{user_location}'").order(score: :desc).limit(5)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :locations, only: :index
+      resources :games, only: :index
     end
   end
 end
