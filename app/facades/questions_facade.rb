d4ecf4aa1a06912e59c12121abeb7f7c7001c791
Chaz Simons <chaz.simons@gmail.com>
2022-01-12 08:43:48 -0800

    Refactors to remove binding.pry and other testing variables

--- app/facades/questions_facade.rb
+++ app/facades/questions_facade.rb
@@ -1,7 +1,7 @@
 class QuestionsFacade
   def self.trivia
     id = 0
-    questions = QuestionsService.trivia[:results].map do |t|
+    QuestionsService.trivia[:results].map do |t|
       id += 1
       t[:id] = "#{id}"
       Question.new(t)
