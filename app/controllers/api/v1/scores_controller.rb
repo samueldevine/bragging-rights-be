e007eb646da8bcca3202f91a3daf9f8088f6f55d
Chaz Simons <chaz.simons@gmail.com>
2022-01-11 17:07:06 -0800

    Test post method with front end data.
    Co-authored-by: Christian
    Co-authored-by: Matthew
    Co-authored-by: Ted

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -10,14 +10,14 @@ class Api::V1::ScoresController < ApplicationController
   def create
     Score.create(score_params)
   end
-  
+
   private
-  
+
   def score_params
     location = LocationsFacade.location_data(params[:score][:ip_address])
     params[:score][:city] = location.city
     params[:score][:state] = location.state
     params[:score][:country] = location.country
-    params.require(:score).permit(:user_id, :score, :city, :state, :country, :game_time)
+    params.require(:score).permit(:user_id, :score, :city, :state, :country)
   end
-end 
+end
