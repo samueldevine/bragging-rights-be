1e8716b318f179f38f611ebac75fe30ad86402f4
Matthew Kimball <mekimba6@gmail.com>
2022-01-13 10:46:31 -0700

    fixes world params and adds some testing for state

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,15 +1,7 @@
 class Score < ApplicationRecord
 
-  # def self.high_score(user)
-  #   where(user_id: user).order(score: :desc).first
-  # end
-  #
-  # def self.top_scores_by_location(geo_scope, user_location)
-  #   where("#{geo_scope} = '#{user_location}'").order(score: :desc).limit(5)
-  # end
-
   def self.top_5_highest_scores(params = {})
-    if params.has_key?(:geo_scope)
+    if params.has_key?(:geo_scope) && params[:geo_scope] != 'world'
       where("#{params[:geo_scope]} = '#{params[:user_location]}'").order(score: :desc).limit(5)
     elsif params.has_key?(:user_id)
       where(user_id: params[:user_id]).order(score: :desc).limit(5)
--- spec/requests/api/v1/scores_request_spec.rb
+++ spec/requests/api/v1/scores_request_spec.rb
@@ -6,25 +6,25 @@ RSpec.describe 'Scores Index' do
                             game_time: 1.5,
                             user_id: 1,
                             city: 'Denver',
-                            state: 'CO',
+                            state: 'Colorado',
                             country: 'United States')
     @score_2 = Score.create(score: 10,
                             game_time: 1.3,
                             user_id: 1,
                             city: 'Denver',
-                            state: 'CO',
+                            state: 'Colorado',
                             country: 'United States')
     @score_3 = Score.create(score: 50,
                             game_time: 1.0,
                             user_id: 2,
                             city: 'Denver',
-                            state: 'CO',
+                            state: 'Colorado',
                             country: 'United States')
     @score_4 = Score.create(score: 100,
                             game_time: 2.2,
                             user_id: 3,
                             city: 'Chicago',
-                            state: 'IL',
+                            state: 'Illinois',
                             country: 'United States')
 
     @score_5 = Score.create(score: 100,
@@ -43,30 +43,45 @@ RSpec.describe 'Scores Index' do
                             game_time: 100,
                             user_id: 5,
                             city: 'Detroit',
-                            state: 'MI',
+                            state: 'Michigan',
                             country: 'United States')
     @score_8 = Score.create(score: 150,
                             game_time: 60.3,
                             user_id: 7,
                             city: 'Chicago',
-                            state: 'IL',
+                            state: 'Illinois',
                             country: 'United States')
     @score_9 = Score.create(score: 250,
                             game_time: 1.5,
                             user_id: 5,
                             city: 'Tampa',
-                            state: 'FL',
+                            state: 'Florida',
                             country: 'United States')
     @score_10 = Score.create(score: 8000,
                              game_time: 4.20,
                              user_id: 69,
                              city: 'Las Vegas',
-                             state: 'NV',
+                             state: 'Nevada',
                              country: 'United States')
   end
 
   it 'can dynamically render top scores by location', :vcr do
     get '/api/v1/scores', params: {ip_address: '73.217.89.89', geo_scope: "city"}
+
+    expect(response).to be_successful
+    scores = JSON.parse(response.body, symbolize_names: true)
+    expect(scores[:data].count).to eq(3)
+
+    scores[:data].each do |score|
+      expect(score[:attributes]).to have_key :user_id
+      expect(score[:attributes][:user_id]).to be_an Integer
+      expect(score[:attributes]).to have_key :score
+      expect(score[:attributes][:score]).to be_an Integer
+      expect(score[:attributes]).to have_key :game_time
+      expect(score[:attributes][:game_time]).to be_a Float
+    end
+    
+    get '/api/v1/scores', params: {ip_address: '73.217.89.89', geo_scope: "state"}
 
     expect(response).to be_successful
     scores = JSON.parse(response.body, symbolize_names: true)
